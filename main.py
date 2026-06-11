70bda07d9fc8de0717bb6c442e657770ed028f99
Ewen <linktriforce2611@gmail.com>
2026-06-11 20:03:52 +0200

    Ajout d'un scrapper simple, html. Sans scructure. Prochain ajout : structure du projet

---
+++ main.py
@@ -0,0 +1,7 @@
+#imports
+import SingleRequest as SR
+
+#SR.gethtml("https://www.leboncoin.fr/recherche?category=14&donation=1")
+
+SR.plz("https://www.leboncoin.fr/recherche?category=14&donation=1")
+print("hello la teams")# le GOAT français, on le laisse.
