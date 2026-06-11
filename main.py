c6f9c7b25969293e060f61a156e5a7160e81054a
Ewen <Ewen>
2026-06-11 22:26:17 +0200

    Retour à 0

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
 #imports
-import SingleRequest as SR
-
-#SR.gethtml("https://www.leboncoin.fr/recherche?category=14&donation=1")
-
-SR.plz("https://www.leboncoin.fr/recherche?category=14&donation=1")
-print("hello la teams")# le GOAT français, on le laisse.
