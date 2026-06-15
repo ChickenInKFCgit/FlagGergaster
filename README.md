3759fd88ec107ebbe60d92f15e8dc6320b6dc15e
Ewen <Ewen>
2026-06-15 23:13:43 +0200

    Première version d'un scrapper qui ouvre une page leboncoin. Cependant, rien n'a été rééllement implémenté pour contourner leur détection, hormis quelques placeholders pour implémenter cela plus tard

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # FlagGergaster
 
-Ce projet met en place un bot discord, qui affiche un dashboard du résultat de son scrapping.
+Ce projet permet de se relier au bot discord, et d'y afficher un dashboard, depuis lequel on peut accéder aux fonctionnalités suivantes :
+
+* Demander le scrapping d'une page selon des filtres très précis
+* Observer le résultat du scrapping, parmis les résultats qui n'étaient pas dans la base de données maison
+* Paramétrer le scrapping selon : la durée avant rafraîchissement, les filtres appliqués, le délai minimal et maximal, etc.
+* Supprimer, Modifier ou Accéder aux résultats stockés
+* Actualiser les annonces, afin de vérifier qu'elles sont toujours disponibles
+
 Le scrapping en question permet de récupérer des annonces postées sur des sites tels que leboncoin, vinted etc.
 
 - Le résultat d'un scrapping est découpé et envoyé dans une base de données "maison" (en tsv)
@@ -8,5 +15,7 @@ Le scrapping en question permet de récupérer des annonces postées sur des sit
 - Le résultat du scrapping est actualisé ponctuellement, ex 24h
 - Le résultat du scrapping est affiché via un dashboard, qui montre les candidats potentiels. L'utilisateur peut choisir de les refuser ce qui va les "shadow-ban".
 
-# Note Personelle
-https://scrape.do/blog/how-to-scrape-javascript-rendered-web-pages-with-python/
+# Documentations utilisées lors du projet
+
+* https://scrape.do/blog/how-to-scrape-javascript-rendered-web-pages-with-python/
+* https://scrapfly.io/blog/posts/web-scraping-with-playwright-and-python
--- scrapper.py
+++ scrapper.py
@@ -1,1 +1,70 @@
-# va scrapper des sites dynamiques qui utilisent javascript. c'est du costaud.
+""" va scrapper des sites dynamiques qui utilisent javascript. c'est du costaud.
+"""
+import time as t
+from random import randint
+
+import nest_asyncio; nest_asyncio.apply()  # This is needed to use sync API in repl
+from playwright.sync_api import sync_playwright
+
+
+import nest_asyncio; nest_asyncio.apply()  # This is needed to use sync API in repl
+from playwright.sync_api import sync_playwright
+
+# Fonctions utilitaires
+def wait(pfDureeMin:int, pfDureeMax:int):
+    duree = randint(pfDureeMin, pfDureeMax) * 0.001
+    t.sleep(duree)
+
+#Scrapper
+class Scrapper:
+    def __init__(self, pfuserdatadir:str= "data/real_real_real_user_data_trust/", pfnavigateur:str="chrome",
+                 pfdelaimin:int=100, pfdelaimax:int=3000 ):
+        """
+        Prends en paramètre:
+        - le chemin du dossier contenant l'historique de recherche, afin de crédibiliser le scrapper
+        - le navigateur à utiliser (par défaut, chrome)
+        - le délai min avant de se déplacer dans le site (en ms)
+        - le délai max avant de se déplacer dans le site (en ms)
+
+        Le délai sera randomisé, mais permet de moins se faire détecter.
+        """
+        self.pw = sync_playwright().start()
+        self.dossier_historique = pfuserdatadir
+        self.navigateur = pfnavigateur
+        self.delaimin = pfdelaimin
+        self.delaimax = pfdelaimax
+
+    
+    def scrap(self, siteURL:str):
+        self.fausser_credibilite_navigateur()
+
+        self.contexte = self.__launch()
+
+        self.simuler_navigation(siteURL)
+        
+
+    def simuler_navigation(self, siteURL:str):
+        page = self.contexte.new_page()
+
+        page.goto(siteURL)
+        print(siteURL.split("/"))
+        t.sleep(10)
+    
+    def fausser_credibilite_navigateur(self):
+        self.fill_user_data_dir()
+         
+
+        
+
+    def fill_user_data_dir(self):
+        pass
+
+    def __launch(self):
+        return self.pw.chromium.launch_persistent_context(
+            user_data_dir=self.dossier_historique,
+            channel="chrome",
+            headless=False
+        )
+
+s = Scrapper()
+s.scrap("https://www.leboncoin.fr/")
